c0adb6effa7a130015ecb776cd3021564023889e
Liz Rea <wizzyrea@gmail.com>
2011-12-06 10:52:56 -0600

    Update bz_tickets.sh

--- bz_tickets.sh
+++ bz_tickets.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
-TCOUNT=bz_count.txt
-URL=bz_urls.txt
-SITE=bz_sites.txt
+WORKINGPATH=~/
+PATH=/var/www
+TCOUNT=$WORKINGPATH/bz_count.txt
+URL=$WORKINGPATH/bz_urls.txt
+SITE=$WORKINGPATH/bz_sites.txt
+COMBINED=$WORKINGPATH/bz_pasted.txt
 
 cat /dev/null > $TCOUNT
 
@@ -10,18 +13,27 @@ do
         wget -q --cookies=on --load-cookies=cookie.txt --keep-session-cookies $URL -O - | grep 'found' | head -1 | sed 's/<[^>]*>//g;s/^[ \t]*//;' >> $TCOUNT
 done
 
-paste -d , $SITE $TCOUNT > bz_pasted.txt
+paste -d , $SITE $TCOUNT > $COMBINED
 
 exec 7<$URL
-exec 8<bz_pasted.txt
+exec 8<$COMBINED
 
-echo "<h1>ByWater Tickets</h1>" > combined.html
-echo "<table border=\"1\">" >> combined.html
+echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"
+    \"http://www.w3.org/TR/html4/loose.dtd\">
+<html>
+<head>
+  <title> ByWater Ticket Counts </title>
+</head>
+<body>
+<h1>ByWater Tickets</h1>
+<table border=\"1\">" >> $PATH/index.html
+
 while read -u 7 f1
 do
     read -u 8 f2
-    echo "<tr><td><a href=\"$f1\">$f2</a></tr></td>" >> combined.html
+    echo "<tr><td><a href=\"$f1\">$f2</a></tr></td>" >> $PATH/index.html
 done
-echo "</table>" >> combined.html
+echo "</table> 
+</body>" >> $PATH/index.html
 
-sudo cp combined.html /var/www/index.html
+
